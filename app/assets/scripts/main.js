62fcb9c8421f51b4815270b6a978cceb183166bf
Vincent <v.rell_home@outlook.com>
2018-09-29 00:53:29 +0200

    installed webpack plugins

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -1,1 +1,1 @@
-alert("Hello");
+import "../styles/main.scss";
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,32 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
-    entry: "./app/assets/scripts/main.js"
+  entry: "./app/assets/scripts/main.js",
+  
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader"
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ]
 }
